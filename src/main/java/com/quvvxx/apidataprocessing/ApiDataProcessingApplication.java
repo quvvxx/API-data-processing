e7ae88dc184982ceeda3cd5637d09e1529490a49
quvvxx <quvvxx2@gmail.com>
2026-08-06 21:28:05 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/quvvxx/apidataprocessing/ApiDataProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.quvvxx.apidataprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDataProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDataProcessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quvvxx/apidataprocessing/ApiDataProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quvvxx.apidataprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDataProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
