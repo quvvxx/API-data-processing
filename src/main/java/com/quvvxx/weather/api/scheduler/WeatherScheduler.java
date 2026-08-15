e8b59fb70bd1885b3ba7be6637f066cdf6f7d11b
quvvxx <quvvxx2@gmail.com>
2026-08-15 16:18:39 +0900

    feat: 스케줄러로 자동 수집 구현

---
+++ src/main/java/com/quvvxx/weather/api/scheduler/WeatherScheduler.java
@@ -0,0 +1,22 @@
+package com.quvvxx.weather.api.scheduler;
+
+import com.quvvxx.weather.api.service.WeatherCollectService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WeatherScheduler {
+
+    private final WeatherCollectService weatherCollectService;
+
+    @Scheduled(cron = "0 10 * * * *")
+    public void collect(){
+        log.info("날씨 수집 시작");
+        weatherCollectService.collect();
+        log.info("날씨 수집 완료");
+    }
+}
